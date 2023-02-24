edeab591967536a946288aa216e3dd80382d6351
Manh Tu VU <glmanhtu@gmail.com>
2023-02-24 12:51:13 +0100

    Update

--- utils/wb_utils.py
+++ utils/wb_utils.py
@@ -12,16 +12,21 @@ def wb_img(image):
     return wandb.Image(reverse_transform()(image))
 
 
-def create_similarity_heatmap(similarity_matrix):
+def create_similarity_heatmap(similarity_matrix, dpi=200):
     # Create the heatmap using seaborn
     heatmap = sns.heatmap(similarity_matrix, cmap="YlGnBu")
 
     # Get the figure object from the heatmap
     fig = heatmap.get_figure()
 
+    # Set the DPI of the matplotlib figure
+    plt.figure(dpi=dpi)
+
     # Convert the figure to a numpy array
     fig.canvas.draw()
     heatmap_array = np.array(fig.canvas.renderer.buffer_rgba())
+
+    # Close the figure to free up memory
     plt.close(fig)
 
     return heatmap_array
