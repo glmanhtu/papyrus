9ceea3a2de0148c4ac8deb33c1b8c86fe0f6abc8
Manh Tu VU <glmanhtu@gmail.com>
2023-03-01 19:06:07 +0100

    Update model

--- scripts/visualise_dataset.py
+++ scripts/visualise_dataset.py
@@ -3,7 +3,6 @@ import logging
 import cv2
 import numpy as np
 import torchvision.transforms
-import tqdm
 
 from dataset.infrared import InfraredDataset
 from dataset.michigan import MichiganDataset
@@ -15,17 +14,15 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s :: %(levelname)s :: 
 args = TrainOptions(save_conf=False).parse()
 transforms = torchvision.transforms.Compose([])
 
-train_dataset = MichiganDataset(args.michigan_dir, transforms, args.image_size)
+train_dataset = InfraredDataset(args.infrared_dir, transforms, args.image_size)
 
-for item in tqdm.tqdm(train_dataset):
-    a = 1 + 2
-    # image = np.concatenate([item['positive'], item['anchor']], axis=0)
-    # # image = cv2.bitwise_not(image)
-    # cv2.imshow('image', cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
-    #
-    # # waitKey() waits for a key press to close the window and 0 specifies indefinite loop
-    # cv2.waitKey(50)
+for item in train_dataset:
+    image = np.concatenate([item['positive'], item['anchor']], axis=0)
+    # image = cv2.bitwise_not(image)
+    cv2.imshow('image', cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+
+    # waitKey() waits for a key press to close the window and 0 specifies indefinite loop
+    cv2.waitKey(50)
 
     # cv2.destroyAllWindows() simply destroys all the windows we created.
-
-print(train_dataset.bad_imgs)
+cv2.destroyAllWindows()
