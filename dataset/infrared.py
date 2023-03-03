a78535444fbffb0fb0d70ce65e5dd30c786dc0fb
Manh Tu VU <mvu@mvu.labri.fr>
2023-03-03 12:50:18 +0100

    Update

--- dataset/infrared.py
+++ dataset/infrared.py
@@ -13,8 +13,30 @@ from utils.data_utils import read_image
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(message)s')
 
-excludes = ['2855e', '2855b', '2882b', '2881c']     # Incorrect segmentation samples
-excludes += ['0567s']   # Too small
+exclude_imgs = ['2969c_IRV', '2969c_IRR', '2934c_IRR', '1306b_IRR', '1353f_IRV', '1353f_IRR', '0567o_IRR',
+                '1250c_IRR', '1250c_IRV', '1224n_IRV', '1224n_IRR', '1317d_IRV', '1317d_IRR', '2841b_IRR',
+                '2841b_IRV', '1445_IRR', '1445_COLR', '1445_IRV', '1445_COLV', '1278c_IRR', '2888e_COLV',
+                '2888e_IRV', '0808b_IRV', '0808b_IRR', '0808e_IRV', '0808e_IRR', '1228a_IRR', '1228a_IRV',
+                '2911b_IRR', '2911b_IRV', '0808c_IRV', '0808c_IRR', '2969e_IRR', '2969e_IRV', '2915_IRV',
+                '2915_IRR', '1322m_IRR', '2996c_IRR', '2996c_IRV', '1290k_IRR', '1290k_IRV', '2930b_IRR',
+                '2930b_IRV', '2916c_IRV', '2916c_IRR', '2949c_IRV', '2949c_IRR', '1438o_IRV', '0808a_IRV',
+                '0808a_IRR', '2913_IRR', '2913_IRV', '2881d_IRV', '2881d_IRR', '2916b_IRV', '2916b_IRR',
+                '1228d_IRV', '1228d_IRR', '1316c_IRR', '1316c_IRV', '1322o_IRV', '1322o_IRR', '2974_IRR',
+                '2974_IRV', '2733e_IRR', '2733e_IRV', '2881c_COLR', '2881c_IRR', '2881c_IRV', '2916a_IRV',
+                '2916a_IRR', '1306h_IRR', '2855e_COLV', '2855e_IRR', '2855e_COLR', '2838g_COLV', '2949a_IRR',
+                '2867f_IRV', '2867f_COLR', '2867f_COLV', '1322k_IRV', '1322k_IRR', '2882b_COLR', '2882b_COLV',
+                '2882b_IRR', '0567s_IRV', '2881b_IRV', '0794_IRV', '0794_IRR', '2969b_IRV', '2733r_IRR', '2733r_IRV',
+                '2735a_IRV', '2735a_IRR', '1220d_1225e_IRR', '2850b_IRR', '2850b_IRV', '0567r_IRV', '0271p_IRV',
+                '0271p_IRR', '2926_IRR', '2926_IRV', '1382i_IRR', '1382i_IRV', '0567o_0567p_0567q_IRV',
+                '0567o_0567p_0567q_IRR', '2849d_IRR', '2849d_IRV', '1322l_IRV', '1322l_COLV', '2969d_IRR',
+                '2969d_IRV', '1207_1209b_IRV', '2970a_IRR', '2970a_IRV', '2853c_IRR', '2975b_IRR', '2975b_IRV',
+                '1223d_IRV', '1290u_IRV', '0779_1257_IRV', '0243_IRV', '0243_IRR', '2855b_COLR',
+                '1317c_1321f_1321k_IRR', '2735i_IRR', '2735i_IRV', '1444a_1444b_IRR', '1444a_1444b_IRV',
+                '2932_IRR', '2932_IRV', '2952b_IRV', '1224i_IRV', '1224i_IRR', '2949b_IRR', '2882c_COLV',
+                '2882c_IRR', '1290r_IRV', '1290r_IRR', '0567n_IRV', '2859a_COLV', '2859a_IRV', '2859a_COLR',
+                '2950b_IRV', '1438g_IRR', '1316d_IRV', '1316d_IRR', '2735j_IRR', '2735j_IRV', '1290w_IRV',
+                '0811c_1442_IRR', '1223e_1224d_IRV', '1223e_1224d_IRR', '3007_IRR', '3007_IRV', '2970b_IRV',
+                '2970b_IRR', '2875f_IRV', '2859b_COLV', '2859b_IRV', '2859b_COLR', '1378i_IRV']
 
 
 def get_fragment_id(file_name):
@@ -75,7 +97,7 @@ class InfraredDataset(Dataset):
             if only_recto and 'COLR' not in file_name:
                 continue
 
-            if file_name.rsplit("_")[0] in excludes:
+            if file_name in exclude_imgs:
                 continue
 
             papyrus_id = self.get_papyrus_id(file_name)
