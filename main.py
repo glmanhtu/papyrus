908b1700b4befdf03eba0e255ffd8533b9833192
VU Manh Tu <glmanhtu@gmail.com>
2024-01-10 21:21:43 +0100

    Update

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import torch
 import torchvision
 from ml_engine.criterion.losses import NegativeCosineSimilarityLoss, DistanceLoss, BatchDotProduct, NegativeLoss
 from ml_engine.criterion.simsiam import BatchWiseSimSiamLoss
+from ml_engine.criterion.triplet import BatchWiseTripletLoss
 
 from ml_engine.data.samplers import DistributedRepeatableEvalSampler, MPerClassSampler
 from ml_engine.engine import Trainer
@@ -23,67 +24,6 @@ import wi19_evaluate
 from datasets.geshaem_dataset import GeshaemPatch, MergeDataset
 from datasets.michigan_dataset import MichiganDataset
 from transforms import RandomSizedCrop
-
-
-class BatchWiseTripletLoss(torch.nn.Module):
-    """
-    Adapted from https://github.com/marco-peer/hip23
-    Triplet loss with negative similarity as distance function
-    """
-    def __init__(self, margin=0.1):
-        super(BatchWiseTripletLoss, self).__init__()
-        self.margin = margin
-
-    def forward(self, emb, target):
-        emb = F.normalize(emb, p=2, dim=-1)
-        return self.forward_impl(emb, target, emb, target)
-
-    def forward_impl(self, inputs_col, targets_col, inputs_row, targets_row):
-        n = inputs_col.size(0)
-        # Compute similarity matrix
-        sim_mat = torch.matmul(inputs_col, inputs_row.t())
-        # split the positive and negative pairs
-        eyes_ = torch.eye(n, dtype=torch.bool).cuda()
-        pos_mask = targets_col.expand(
-            targets_row.shape[0], n
-        ).t() == targets_row.expand(n, targets_row.shape[0])
-        neg_mask = ~pos_mask
-        pos_mask[:, :n] = pos_mask[:, :n] * ~eyes_
-
-        loss = list()
-        neg_count = list()
-        for i in range(n):
-            pos_pair_idx = torch.nonzero(pos_mask[i, :]).view(-1)
-            if pos_pair_idx.shape[0] > 0:
-                pos_pair_ = sim_mat[i, pos_pair_idx]
-                pos_pair_ = torch.sort(pos_pair_)[0]
-
-                neg_pair_idx = torch.nonzero(neg_mask[i, :]).view(-1)
-                neg_pair_ = sim_mat[i, neg_pair_idx]
-                neg_pair_ = torch.sort(neg_pair_)[0]
-
-                select_pos_pair_idx = torch.nonzero(
-                    pos_pair_ < neg_pair_[-1] + self.margin
-                ).view(-1)
-                pos_pair = pos_pair_[select_pos_pair_idx]
-
-                select_neg_pair_idx = torch.nonzero(
-                    neg_pair_ > max(0.6, pos_pair_[-1]) - self.margin
-                ).view(-1)
-                neg_pair = neg_pair_[select_neg_pair_idx]
-
-                pos_loss = torch.sum(1 - pos_pair)
-                if len(neg_pair) >= 1:
-                    neg_loss = torch.sum(neg_pair)
-                    neg_count.append(len(neg_pair))
-                else:
-                    neg_loss = 0
-                loss.append(pos_loss + neg_loss)
-            else:
-                loss.append(0)
-
-        loss = sum(loss) / n
-        return loss
 
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
@@ -107,16 +47,12 @@ class GeshaemTrainer(Trainer):
         img_size = data_cfg.img_size
         if mode == 'train':
             return torchvision.transforms.Compose([
-                torchvision.transforms.RandomApply([
-                    RandomSizedCrop(min_width=224, min_height=224, pad_if_needed=True, fill=(255, 255, 255)),
-                ]),
                 torchvision.transforms.RandomCrop(512, pad_if_needed=True, fill=(255, 255, 255)),
                 torchvision.transforms.RandomHorizontalFlip(),
                 torchvision.transforms.RandomVerticalFlip(),
                 ACompose([
                     A.CoarseDropout(max_holes=16, min_holes=1, min_height=16, max_height=128, min_width=16,
-                                    max_width=128,
-                                    fill_value=255, always_apply=True),
+                                    max_width=128, fill_value=255, always_apply=True),
                 ]),
                 torchvision.transforms.Resize(img_size),
                 torchvision.transforms.RandomApply([
